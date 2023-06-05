6a649f064b3e5862ecaad794e6db1a1c3ca01d42
[R*] Dante <102482809+unknownkillid@users.noreply.github.com>
2023-06-06 03:03:41 +0400

    display added

--- JavaScript/index.js
+++ JavaScript/index.js
@@ -12,3 +12,42 @@ setTimeout(() => {
         desktop.classList.add('displayFlex')
     }, 5000);
 }, 5000);
+
+
+const deployWindow = document.getElementById("deploy-window");
+
+function handleRightClick(event) {
+  event.preventDefault();
+
+  deployWindow.style.display = "block";
+  deployWindow.style.left = event.pageX + "px";
+  deployWindow.style.top = event.pageY + "px";
+}
+
+function hideDeployWindow() {
+  deployWindow.style.display = "none";
+}
+
+document.addEventListener("contextmenu", handleRightClick); 
+document.addEventListener("click", hideDeployWindow); 
+
+
+const properties = document.getElementById('properties')
+let propOpened = false;
+
+function Properties(event) {
+  event.preventDefault();
+
+  if (!propOpened) {
+    properties.style.display = "flex";
+    properties.style.left = event.pageX + "px";
+    properties.style.top = event.pageY - 300 + "px";
+  }
+}
+
+function xButton(){
+  properties.style.display = 'none';
+}
+
+const propopen = document.getElementById('propopen').addEventListener('click', Properties)
+const propXButton = document.getElementById('propXButton').addEventListener('click', xButton)
