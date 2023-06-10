521de59730c3796f5c40199eace739e13e99ed55
[R*] Dante <102482809+unknownkillid@users.noreply.github.com>
2023-06-11 00:53:59 +0400

    browser error added

--- JavaScript/index.js
+++ JavaScript/index.js
@@ -1,17 +1,20 @@
-// const startScreen = document.getElementById('startUp')
-// const termianl = document.getElementById('terminal')
+const startScreen = document.getElementById('startUp')
+const termianl = document.getElementById('terminal')
 const desktop = document.getElementById('desktop')
 
+function loading() {
+  setTimeout(() => {
+    startScreen.classList.add('displayNone')
+    termianl.classList.add('displayBlock')
 
-// setTimeout(() => {
-//     startScreen.classList.add('displayNone')
-//     termianl.classList.add('displayBlock')
+    setTimeout(() => {
+      termianl.classList.remove('displayBlock')
+      desktop.classList.add('displayFlex')
+    }, 5000);
+  }, 5000);
+}
 
-//     setTimeout(() => {
-//         termianl.classList.remove('displayBlock')
-//         desktop.classList.add('displayFlex')
-//     }, 5000);
-// }, 5000);
+loading();
 
 
 const deployWindow = document.getElementById("deploy-window");
@@ -28,8 +31,8 @@ function hideDeployWindow() {
   deployWindow.style.display = "none";
 }
 
-document.addEventListener("contextmenu", handleRightClick); 
-document.addEventListener("click", hideDeployWindow); 
+document.addEventListener("contextmenu", handleRightClick);
+document.addEventListener("click", hideDeployWindow);
 
 
 const properties = document.getElementById('properties')
@@ -45,40 +48,41 @@ function Properties(event) {
   }
 }
 
-function xButton(){
+function xButton() {
   properties.style.display = 'none';
 }
 
 const propopen = document.getElementById('propopen').addEventListener('click', Properties)
 const propXButton = document.getElementById('propXButton').addEventListener('click', xButton)
 
-$(document).ready(function() {
+$(document).ready(function () {
   var isDragging = false;
   var startPosX;
   var startPosY;
   var startWindowPosX;
   var startWindowPosY;
 
-  $("#displayHeader").mousedown(function(event) {
-      isDragging = true;
-      startPosX = event.pageX;
-      startPosY = event.pageY;
-      startWindowPosX = $("#properties").offset().left;
-      startWindowPosY = $("#properties").offset().top;
+  $(".moved").mousedown(function (event) {
+    isDragging = true;
+    startPosX = event.pageX;
+    startPosY = event.pageY;
+    var $element = $(this);
+    startWindowPosX = $element.offset().left;
+    startWindowPosY = $element.offset().top;
   });
 
-  $(document).mousemove(function(event) {
-      if (isDragging) {
-          var offsetX = event.pageX - startPosX;
-          var offsetY = event.pageY - startPosY;
-          var newWindowPosX = startWindowPosX + offsetX;
-          var newWindowPosY = startWindowPosY + offsetY;
-          $("#properties").offset({ top: newWindowPosY, left: newWindowPosX });
-      }
+  $(document).mousemove(function (event) {
+    if (isDragging) {
+      var offsetX = event.pageX - startPosX;
+      var offsetY = event.pageY - startPosY;
+      var newWindowPosX = startWindowPosX + offsetX;
+      var newWindowPosY = startWindowPosY + offsetY;
+      $(".moved").offset({ top: newWindowPosY, left: newWindowPosX });
+    }
   });
 
-  $(document).mouseup(function() {
-      isDragging = false;
+  $(document).mouseup(function () {
+    isDragging = false;
   });
 });
 
@@ -146,9 +150,9 @@ document.addEventListener('DOMContentLoaded', () => {
 let timeFooter = document.getElementById("currentTime");
 
 function updateTime() {
-    let currentTime = new Date();
-    let formattedTime = currentTime.toLocaleTimeString([], { hour: '2-digit', minute: '2-digit' });
-    timeFooter.textContent = formattedTime;
+  let currentTime = new Date();
+  let formattedTime = currentTime.toLocaleTimeString([], { hour: '2-digit', minute: '2-digit' });
+  timeFooter.textContent = formattedTime;
 }
 
 updateTime();
@@ -165,3 +169,68 @@ const minimizeExplorer = document.getElementById('minimizeExplorer').addEventLis
 const quitExplorer = document.getElementById('quitExplorer').addEventListener('click', () => {
   explorerMain.style.display = 'none';
 })
+
+let flag = false;
+function resized() {
+  document.querySelectorAll('.size').forEach(allWindows => {
+    document.querySelectorAll('.resizeClick').forEach(reiszeButtons => {
+      reiszeButtons.addEventListener('click', () => {
+        if (!flag) {
+          allWindows.classList.add('widthresizeSmaller');
+          allWindows.classList.remove('windowresizeBigger')
+          flag = true;
+        } else {
+          flag = false;
+          allWindows.classList.remove('widthresizeSmaller')
+          allWindows.classList.add('windowresizeBigger')
+        }
+      })
+    })
+  })
+}
+
+resized()
+
+
+
+const errorSound = new Audio();
+errorSound.src = 'assets/sounds/error.mp3'
+let soundPlayed = false;
+
+const errorWindow = document.getElementById('error')
+
+document.getElementById('explorerInput').addEventListener('keydown', ({ keyCode }) => {
+  switch (keyCode) {
+    case 13:
+      if (!soundPlayed){
+        errorSound.play();
+        soundPlayed = true;
+      }
+      errorWindow.classList.add('displayFlex')
+      document.getElementById('errorOk').addEventListener('click', () => {
+       restartError();
+      })
+      
+      break;
+
+    default:
+      break;
+  }
+})
+
+
+function restartError() {
+  startScreen.classList.remove('displayNone')
+  explorerMain.style.display = 'none';
+  errorWindow.classList.remove('displayFlex')
+  soundPlayed = false;
+  setTimeout(() => {
+    startScreen.classList.add('displayNone')
+    termianl.classList.add('displayBlock')
+
+    setTimeout(() => {
+      termianl.classList.remove('displayBlock')
+      desktop.classList.add('displayFlex')
+    }, 5000);
+  }, 5000);
+}
